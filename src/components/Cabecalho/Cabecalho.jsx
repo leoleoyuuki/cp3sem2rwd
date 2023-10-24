768388f83766a67afb8f51135b318ea5e4f1add3
leoleoyuuki <rm550373@fiap.com.br>
2023-10-23 21:48:45 -0300

    Cabecalho separado

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -4,12 +4,14 @@ import React from 'react'
 export default function Cabecalho() {
   return (
     <header className="bg-gray-800 text-white ">
+      
           <nav className='flex justify-around items-center h-16'>
             <Link className='linkCabecalho' href="/item/camisa">Camisa</Link>
             <Link className='linkCabecalho' href="/item/calca">Calca</Link>
             <Link className='linkCabecalho' href="/item/sapato">Sapato</Link>
             <Link className='linkCabecalho' href="/item/oculos">Oculos</Link>
             <Link className='linkCabecalho' href="/item/bone">Bone</Link>
+
           </nav>
         </header>
   )

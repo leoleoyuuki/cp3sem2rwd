43c73fb13bdef75f56896347a2156de177caf24b
leoleoyuuki <rm550373@fiap.com.br>
2023-10-23 21:55:45 -0300

    404 img added

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Image from "next/image";
 
 export default function NotFound() {
   return (
     <div>
         <h1>404 - Page Not Found</h1>
         <p>Desculpa a pagina não foi encontrada</p>
+        <figure>
+          <Image src="/img/erro.jpg" alt="Armario" width={320} height={320}></Image>
+        </figure>
     </div>
   )
 }

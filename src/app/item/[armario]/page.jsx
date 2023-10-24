fd4275d8a0d0f2c18ee295de0e0dc966e66fc224
leoleoyuuki <rm550373@fiap.com.br>
2023-10-23 21:51:40 -0300

    Organizando a estrutura

--- src/app/item/[armario]/page.jsx
+++ src/app/item/[armario]/page.jsx
@@ -4,13 +4,10 @@ import Image from "next/image";
 export default function Camisa({params}) {
   return (
     <div className='bg'>
-        
         <figure className='flex flex-col items-center justify-center' >
-
             <Image src={`/img/${params.armario}.jpg`} alt="Armario" width={320} height={320}></Image>
             <h1 className='text-2xl font-bold'>{params.armario}</h1>
         </figure>
-        
     </div>
   )
 }
